7baa324c86d06e0c714d39217358f07f0091dae0
DarkMan9 <mandar.kulkarni799@gmail.com>
2018-04-11 14:32:26 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+const app = express();
+
+
+app.set('view engine','hbs');
+
+var port = process.env.PORT || 3000;
+
+hbs.registerPartials(__dirname+'/views/partials');
+
+hbs.registerHelper('getCurrentYear',() => {
+    return new Date().getFullYear();
+});
+
+
+hbs.registerHelper('screamIt',(screamText) => {
+    return screamText;
+
+});
+
+app.use((req,res,next) => {
+    
+    var now = new Date().toString();
+
+    var log = `${now} : ${req.method} ${req.url}`;
+    
+    fs.appendFile('server.log',log+'\n',(err) => {
+        if(err)
+        {
+            console.log('unable to append server.log ');
+        }
+        
+    
+    });
+    
+    console.log(log);
+
+    
+    next();
+
+});
+
+// app.get('/',(req,res) => {
+//     res.send({
+//         name:'Mandar Kulkarni',
+//         age:26,
+//         hobbies:[
+//             'novels',
+//             'ps4',
+//             'x-23'
+//         ]
+//     });
+// });
+
+app.get('/home',(req,res) => {
+    res.render('home.hbs',{
+        pageTitle:'Home Page',
+        currentYear:new Date().getFullYear(),
+        welcomeText:'welcome to Nodejs'
+    })
+});
+
+
+app.get('/about',(req,res) => {
+    
+    res.render('about.hbs',{
+        pageTitle:'About Page',
+        currentYear:new Date().getFullYear()
+
+    });
+
+});
+
+app.listen(port, () => {
+    console.log('server is listening @localhost:3000');
+
+});
